f4c98ae5864b4712937c413b282c5177b2f631f0
Zach Johnson <zachjjohns@gmail.com>
2021-07-28 12:22:26 -0500

    Start to build out StoryList view

--- src/StoryList/StoryList.js
+++ src/StoryList/StoryList.js
@@ -1,11 +1,20 @@
 import "./StoryList.css";
-
+import StoryCard from '../StoryCard/StoryCard';
 import React from 'react'
 
-export default function StoryList() {
+export default function StoryList({ stories }) {
+  const storyCards = stories.map(story => {
+    return (
+      <StoryCard
+        id={story.uri}
+        imgLarge={story.multimedia[0].url}
+        imgThumb={story.multimedia[3].url}
+      />
+    )
+  })
   return (
     <>
-      <h1>Story List View</h1>
+      {storyCards}
     </>
   )
 }
