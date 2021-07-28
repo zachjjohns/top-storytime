7946101bc716a4fad7b0ec2328c95d10af80b6fd
Zach Johnson <zachjjohns@gmail.com>
2021-07-28 18:05:49 -0500

    Wrap index.js in BrowserRouter

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Header from '../Header/Header';
 import StoryList from '../StoryList/StoryList';
+import StoryDetails from '../StoryDetails/StoryDetails';
 import { getHomeStories } from '../apiCalls';
 import { Switch, Route } from 'react-router-dom';
 import React, { Component } from 'react'
@@ -28,7 +29,17 @@ export default class App extends Component {
     return (
       <>
         <Header />
-        <StoryList stories={this.state.stories}/>
+          <Switch>
+            <Route exact path='/'>
+              <StoryList stories={this.state.stories} />
+            </Route>
+            <Route
+            exact path="/:id"
+            render={({ match }) => {
+              const id  = match.params.id;
+              return <StoryDetails id={id} />
+            }}/>
+          </Switch>
       </>
     )
   }
