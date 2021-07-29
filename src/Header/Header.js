f282d026e02c050c6a4435c528ccfba3bf5cc440
Zach Johnson <zachjjohns@gmail.com>
2021-07-29 14:59:51 -0500

    Add search input to header

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -2,10 +2,14 @@ import "./Header.css";
 
 import React from 'react'
 
-export default function Header() {
+export default function Header({ searchValue, handleChange }) {
   return (
     <header>
       <h1 className="app-title">Top StoryTime</h1>
+      <input type="search"
+      placeholder="Search Top Stories"
+      value={searchValue}
+      onChange={(event) => handleChange(event)} />
     </header>
   )
 }
