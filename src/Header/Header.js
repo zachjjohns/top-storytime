e5dcf110c54eb76d92a2cefb287a1cd12b47631f
Zach Johnson <zachjjohns@gmail.com>
2021-07-29 14:45:56 -0500

    Build out StoryDetails view

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -5,7 +5,7 @@ import React from 'react'
 export default function Header() {
   return (
     <header>
-      <h1>Top StoryTime</h1>
+      <h1 className="app-title">Top StoryTime</h1>
     </header>
   )
 }
